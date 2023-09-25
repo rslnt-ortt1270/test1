2cf72b23af61affe34fdfbeb32ef64d3a59dbbfb
Alrrep <perlaa.rmn@gmail.com>
2023-09-25 13:25:11 -0600

    try except implements

--- example1.py
+++ example1.py
@@ -2,7 +2,20 @@
 #by alumnos@ENES-FC2006-16
 #GNU/GPL License
 import numpy as np
-x= np.pi
-z= x**2
+data_list = np.arangel(1,11,1)
+for x in data_list:
+    print("value: " + str(x))
+    
+x= "hola"
+z= 0
+
+try:
+    z = int(x) +1
+    print ("ok")
+except:
+    print("Error in sum of z"
 print (z)
+
+
+    
 
